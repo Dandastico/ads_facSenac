23fac377aba71858a3f77f75cb6e46f84b16e140
dandastico <dandastico.bsb@gmail.com>
2025-03-26 10:16:11 -0300

    resolução de par_impar

--- 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula06/js/par_impar.js
+++ 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula06/js/par_impar.js
@@ -3,5 +3,15 @@ console.log("JS foi carregado")
 
 // criar função
 function parImpar(event) {
-    
+    // impedir envio de dados para servidor
+    event.preventDefault();
+
+    const n = Number(document.getElementById("numero").value)
+    console.log(n);
+
+    let resultado = (n%2==0)? "Par":"Ímpar";
+
+    const output = document.querySelector("h3")
+    output.innerHTML = `${n} é um número ${resultado}`
+
 }
