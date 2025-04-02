2d9ea6476a4617f9630e0668c7980f2a885e065d
dandastico <dandastico.bsb@gmail.com>
2025-04-02 10:28:43 -0300

    começo do fim

---
+++ 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula07/js/aluno.js
@@ -0,0 +1,10 @@
+// ver se JS funcionou
+console.log("JS carregou")
+
+function Aluno(nome, disciplina, frequencia,nota1, nota2) {
+    this.nome = nome,
+    this.disciplina = disciplina,
+    this.frequencia = frequencia,
+    this.nota1 = nota1,
+    this.nota2 = nota2
+}
