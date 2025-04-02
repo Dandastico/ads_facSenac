e5b69f33c6df49785427677f5df05c567a40c777
dandastico <dandastico.bsb@gmail.com>
2025-04-02 11:07:34 -0300

    apanhando do exercício

--- 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula07/js/aluno.js
+++ 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula07/js/aluno.js
@@ -8,3 +8,29 @@ function Aluno(nome, disciplina, frequencia,nota1, nota2) {
     this.nota1 = nota1,
     this.nota2 = nota2
 }
+
+function calcularMedia(event){
+    // impedir envio de dados pro servidor
+    event.preventDefault();
+
+    // pegar nome do formulário
+    const nome = document.getElementById("nome").value
+    console.log(nome)
+
+    // pegar disciplina
+    const disciplina = document.getElementById("disciplina").value
+    console.log(disciplina)
+
+    // pegar porcentagem de frequência
+    const frequencia = document.getElementById("frequencia").value
+    console.log(frequencia)
+
+    // pegar notas do formulário
+    const nota1 = Number(document.getElementById("nota1").value)
+    console.log(nota1)
+
+    const nota2 = Number(document.getElementById("nota2").value)
+    console.log(nota2)
+
+    const aluno = new Aluno(nome, disciplina, frequencia, nota1, nota2);
+}
