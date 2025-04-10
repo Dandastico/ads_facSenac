43240a00a12d8b2d06a7e8bbb32036d647d019d6
Dandastico <dandastico.bsb@gmail.com>
2025-04-10 19:34:52 -0300

    a função asc deu certo no exercício dois da lista de exercícios de JS

--- 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula08/listaExercicios_DanielFerreira/exercicio02.js
+++ 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula08/listaExercicios_DanielFerreira/exercicio02.js
@@ -1,5 +1,12 @@
+// quero ver se js está carregando
+console.log("JS carregou!")
+
+
 // função que imprime na ordem ascendente
 function asc() {
+    // teste pra ver se foi invocada corretamente
+    console.log("Função ativada")
+
     let less = parseInt(document.getElementById("start").value);
     let more = parseInt(document.getElementById("end").value);
 
@@ -13,9 +20,27 @@ function asc() {
     function render(value) {
         txt += value + ' '
     }
-    
     numbers.forEach(render);
 
+    // demonstrar string no HTML
     let resp = document.getElementById("resposta");
     resp.innerHTML = txt
 }
+
+
+// função que vai imprimir em ordem decrescente
+function desc() {
+    // teste pra ver se foi invocada corretamente
+    console.log("Funcão desc foi invocada!")
+
+    let less = parseInt(document.getElementById("start").value);
+    let more = parseInt(document.getElementById("end").value);
+}
+
+
+// cria escutadores de eventos pro botões HTML e os vincula à funções
+document.addEventListener('DOMContentLoaded', function() {
+    // ao clicar no botão com id específica, rode a função correspondente
+    document.getElementById('asc').addEventListener('click', asc);
+    document.getElementById('desc').addEventListener('click', desc);
+});
