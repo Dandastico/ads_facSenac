644d86bd44882c5cd2a456216a6c0aa8b3bec44e
Dandastico <dandastico.bsb@gmail.com>
2025-04-11 21:12:12 -0300

    apanhando exercício 04

--- 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula08/listaExercicios_DanielFerreira/exercicio04.js
+++ 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula08/listaExercicios_DanielFerreira/exercicio04.js
@@ -1,18 +1,26 @@
 console.log("JS 04 carregou!")
 
 
+// inicializar fila como variável global
+let fila = [];
+
+
 // criar funções que introduz nome digitado no array de clientes
 function entrarFila() {
     console.log("Função entrarFila ativada!")
 
     // inicializar variáveis
-    let fila = [];
     let nome = document.getElementById("txt").value;
     console.log(nome);
 
+    // adicionar nome na última posição do array
+    fila.push(nome);
+    return fila
 }
 
-
+// mostrar fila na tela
+let resp = document.getElementById("resposta");
+resp.innerHTML = fila;
 
 
 // vou criar os observadores de eventos para cada botão
