8929acca1ba14d8bcb69e3f02c8196688b26db8d
Dandastico <dandastico.bsb@gmail.com>
2025-04-11 22:21:12 -0300

    VITÓRIA! Venci o ex04 da lista JS

--- 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula08/listaExercicios_DanielFerreira/exercicio04.js
+++ 2025.01_segundoSemestre/2025.01_lingProgWeb/lingProgWeb_aula08/listaExercicios_DanielFerreira/exercicio04.js
@@ -3,6 +3,7 @@ console.log("JS 04 carregou!")
 // inicializar variáveis
 let fila = [];
 let resp = document.getElementById("resposta");
+resp.innerHTML = "Fila vazia";
 
 
 // criar funções que introduz nome digitado no array de clientes
@@ -13,21 +14,35 @@ function entrarFila() {
     let nome = document.getElementById("txt").value;
     console.log(nome);
 
-    // adicionar nome na última posição do array
-    fila.push(nome);
+    // só adiciona nome e imprime array se usuário digitou algo
+    if (nome) {
+        fila.push(nome);
 
-    // mostrar fila na tela
-    resp.innerHTML = fila;
+        // limpar input
+        document.getElementById("txt").value = '';
+
+        // mostrar fila na tela
+        resp.innerHTML = fila.join(", ");
+    }
 }
 
 
 // criar função que retira o primeiro nome do array de clientes
-function  sairFila(fila) {
-    comnsole.log("Função sairFila ativada!");
+function  sairFila() {
+    console.log("Função sairFila ativada!");
 
-    // retirar primeiro nome do array
-    fila.shift();
-    console.log(fila);
+    // retira primeiro nome apenas se array tiver pelo menos um elemento
+    if (fila.length > 0){
+        fila.shift();
+        console.log(fila);
+
+        // atualizar exibição da fila
+        if (fila.length > 0) {
+            resp.innerHTML = fila.join(", ");
+        } else {
+            resp.innerHTML = "Fila vazia";
+        }
+    }
 }
 
 
