fcd2e7150a3244d6cd1b328055426d3944ca7418
dandastico <dandastico.bsb@gmail.com>
2025-05-08 11:33:28 -0300

    métodos e funções de pandas

--- 2025.01_segundoSemestre/2025.01_lingTecProg/2025.01_lingTecProg_aula11/aula11_pandas.py
+++ 2025.01_segundoSemestre/2025.01_lingTecProg/2025.01_lingTecProg_aula11/aula11_pandas.py
@@ -2,5 +2,13 @@ import pandas as pd
 
 # biblioteca pandas tem múltiplos métodos para ler arquivos
 dados = pd.read_csv("Titanic-Dataset.csv")
-print(dados)
-print(dados.info())
+
+print(dados)    # imprime o arquivo csv como tabela
+
+print(dados.info())    # imprime algo parecido com schema
+
+print(dados.describe())    # imprime uma estatística básica sobre os dados
+
+dados.dropna()    # remove os dados nulos
+
+dados.drop_duplicates()    # remove dados duplicados
