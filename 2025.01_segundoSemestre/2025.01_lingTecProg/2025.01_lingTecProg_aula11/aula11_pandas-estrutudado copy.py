757121c03f61f8d17d2f5519e2dbb8291778b787
dandastico <dandastico.bsb@gmail.com>
2025-05-15 11:39:37 -0300

    aula 11

--- 2025.01_segundoSemestre/2025.01_lingTecProg/2025.01_lingTecProg_aula11/aula11_pandas-estrutudado copy.py
+++ 2025.01_segundoSemestre/2025.01_lingTecProg/2025.01_lingTecProg_aula11/aula11_pandas-estrutudado copy.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
+# carregar arquivo csv
 def carregarDados(nomeArquivo):
-    # biblioteca pandas tem múltiplos métodos para ler arquivos
     try:
+        # biblioteca pandas tem múltiplos métodos para ler arquivos
         dados = pd.read_csv(nomeArquivo)
         print(dados)    # imprime o arquivo csv como tabela
     except:
@@ -12,13 +13,27 @@ def carregarDados(nomeArquivo):
     return None
 
 
+# imprime atributos do objeto e retirar algumas linhas não interessantes
 def prepararDados(dados):
     print(dados.info())    # imprime algo parecido com schema
     print(dados.describe())    # imprime uma estatística básica sobre os dados
 
-    # métodos que alteram valores "inplace", direto na memória
-    dados.dropna()    # remove os dados nulos
-    dados.drop_duplicates()    # remove dados duplicados
+    print(dados.head())    # recupera os 5 primeiros registros
+    print(dados.tail())    # recupera os 5 últimos registros (as tranqueiras costumam ficar no final)
+
+    #remove uma coluna do data frama em memória, de modo local
+    dados.drop(columns=["Cabin"], inplace=True)
+
+    dados = dados.dropna()    # remove os dados nulos
+    dados = dados.drop_duplicates()    # remove dados duplicados
+    
+    '''
+    for linha in dados.values:
+        for coluna in linha:
+            print(coluna)
+    
+    Consumo quadráditico, consome muitos recursos
+    '''
 
     return dados
 
