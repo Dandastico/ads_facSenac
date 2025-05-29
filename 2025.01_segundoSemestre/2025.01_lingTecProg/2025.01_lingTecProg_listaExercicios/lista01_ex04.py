aab627748728bc52b55689c02b900fa9cd621b39
dandastico <dandastico.bsb@gmail.com>
2025-05-29 10:42:51 -0300

    RESOLUÇÃO: ex 04, lista 01

---
+++ 2025.01_segundoSemestre/2025.01_lingTecProg/2025.01_lingTecProg_listaExercicios/lista01_ex04.py
@@ -0,0 +1,8 @@
+def main():
+    nums = [1,2,3,4,5,6,7,8,9,10]
+    print(f"O maior número da lista é {max(nums)}")
+    print(f"A soma de todos valores na lista é {sum(nums)}")
+
+
+if __name__ == "__main__":
+    main()
