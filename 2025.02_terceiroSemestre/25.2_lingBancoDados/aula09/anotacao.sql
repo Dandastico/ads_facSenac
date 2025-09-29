3884f16f8801767a34032b4f5df6f29f1b357338
dandastico <danadstico.bsb@gmail.com>
2025-09-29 20:44:51 -0300

    exercícios em sala

--- 2025.02_terceiroSemestre/25.2_lingBancoDados/aula09/anotacao.sql
+++ 2025.02_terceiroSemestre/25.2_lingBancoDados/aula09/anotacao.sql
@@ -1,9 +1,46 @@
 USE TURMAB;
 
+SELECT *
+FROM PRODUTOS;
+
 /*
-FUNÇÕES DE AGRAGAÇÃ
-1 - O gerente precisa saber o total de venda em reais
+FUNÇÕES DE AGRAGAÇÃO
+O gerente precisa saber:
+1 - o total de venda em reais - SUM()
+2 - o valor médio de vendas em reais - AVG()
+3 - a venda com maior valor - MAX()
+4 - a venda com menor valor - MIN()
+5 - quantidade de vendas - COUNT()
 */
 
+-- função soma
 SELECT SUM(valor) FROM PRODUTOS
 WHERE status_venda = 'Vendido';
+
+-- média
+SELECT AVG(valor) FROM PRODUTOS
+WHERE status_venda = 'Vendido';
+
+-- maior valor
+SELECT MAX(valor) FROM PRODUTOS
+WHERE status_venda = 'Vendido';
+
+-- menor valor
+SELECT MIN(valor) FROM PRODUTOS
+WHERE status_venda = 'Vendido';
+
+-- contagem das linhas
+SELECT COUNT(*) FROM PRODUTOS
+WHERE status_venda = 'Vendido';
+
+/*
+AGRUPAMENTO DE DADOS
+8 - quantidade total de vendas por ano - GROUP BY
+*/
+
+SELECT ano_aquisicao, COUNT(*) AS quant_vendas
+FROM PRODUTOS
+WHERE status_venda = 'Vendido'
+GROUP BY ano_aquisicao
+ORDER BY ano_aquisicao;
+
---
+++ 2025.02_terceiroSemestre/25.2_lingBancoDados/aula09/exercicios_produtosDB.sql
@@ -0,0 +1,44 @@
+/*
+EXERCÍCIOS
+1- Informe quantos celulares foram vendidos por ano
+2- Informe o produto mais barato vendido por ano
+3- Informe a média de vendas por ano
+4- Informe a soma de vendas por ano
+5- Informe quantos produtos estão disponíveis para venda
+*/
+
+SELECT * FROM PRODUTOS;
+
+-- quantos celulares foram vendidos por ano
+SELECT ano_aquisicao, COUNT(*) AS celulares_vendidos
+FROM PRODUTOS
+WHERE status_venda = 'Vendido' AND
+categoria = 'Celular'
+GROUP BY ano_aquisicao
+ORDER BY ano_aquisicao;
+
+-- produto mais barato vendido por ano
+SELECT ano_aquisicao, MIN(valor) AS celular_mais_barato
+FROM PRODUTOS
+WHERE status_venda = 'Vendido'
+GROUP BY ano_aquisicao
+ORDER BY ano_aquisicao;
+
+-- média de vendas por ano
+SELECT ano_aquisicao, AVG(valor) AS media_vendas
+FROM PRODUTOS
+WHERE status_venda = 'Vendido'
+GROUP BY ano_aquisicao
+ORDER BY ano_aquisicao;
+
+-- soma de vendas por ano
+SELECT ano_aquisicao, SUM(valor) AS soma_das_vendas
+FROM PRODUTOS
+WHERE status_venda = 'Vendido'
+GROUP BY ano_aquisicao
+ORDER BY ano_aquisicao;
+
+-- quantos produtos disponíveis para venda
+SELECT COUNT(*) AS quant_produtos_disponiveis
+FROM PRODUTOS
+WHERE status_venda = 'Disponível';
