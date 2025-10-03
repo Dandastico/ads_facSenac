0dca268fe17a3bcee7350323647c750312a2f691
Daniel Ferreira Alves <Daniel55761076@edu.df.senac.br>
2025-10-03 19:58:52 -0300

    Aula de Java

---
+++ 2025.02_terceiroSemestre/25.2_lingPOO/aula08/Matriz.java
@@ -0,0 +1,32 @@
+public class Matriz {
+    public static void main(String[] args) {
+        // armaazena valor na matriz
+        int[][] matriz = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        // percorer a matriz pela linha primeiro
+        for (int i = 0; i < matriz.length; i++)
+        {
+            for (int j = 0; j < matriz[0].length; j++)
+            {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(); // pular linha
+        }
+        System.out.println(); // pula linha ao terminar de imprimir 
+
+        // percorrer pelas colunas
+        for (int j = 0; j < matriz[0].length; j++)
+        {
+            for (int i = 0; i < matriz.length; i++)
+            {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    } // fim main
+}
