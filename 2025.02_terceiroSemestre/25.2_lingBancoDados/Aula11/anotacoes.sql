e2285abdf360abf460b2facdf493af36f344d13c
dandastico <dandastico.bsb@gmail.com>
2025-10-13 19:25:18 -0300

    inicio da aula ling db

---
+++ 2025.02_terceiroSemestre/25.2_lingBancoDados/Aula11/anotacoes.sql
@@ -0,0 +1,10 @@
+use TURMAB;
+
+SELECT * FROM PRODUTOS;
+SELECT * FROM CLIENTES;
+SELECT * FROM VENDAS;
+
+-- O gerente precisa saber a marca,
+-- o modelo, a categoria, o cpf, o valor do produto
+-- e o valor da venda de todas as vendas que foram realizadas
+
