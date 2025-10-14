6c5234b2e2e504c0c810f0290c9ff3dd1c18d797
dandastico <dandastico.bsb@gmail.com>
2025-10-13 21:18:52 -0300

    peixe

--- 2025.02_terceiroSemestre/25.2_lingBancoDados/Aula11/exercicios.sql
+++ 2025.02_terceiroSemestre/25.2_lingBancoDados/Aula11/exercicios.sql
@@ -10,3 +10,71 @@ SELECT * FROM VENDAS;
     4 - VALOR DE VENDA DE TODOS OS PRODUTOS DA LG VENDIDOS NA LOJA
 */
 
+-- 1 - NOME, O CPF, O TELEFONE E O EMAIL DE CLIENTES QUE FIZERAM COMPRAS ACIMA DE 5 MIL REAIS
+SELECT
+	c.nome,
+    c.cpf,
+    c.email,
+    v.valor
+FROM 
+	CLIENTES c
+INNER JOIN
+	VENDAS v
+ON c.cpf = v.cpf_cliente
+WHERE v.valor > 5000;
+
+-- 2 - NOME, CPF E TELEFONE DE CLIENTES QUE FIZERAM COMPRAS EM 2025
+SELECT
+	c.nome,
+    c.cpf,
+    c.telefone
+FROM
+	CLIENTES c
+INNER JOIN
+	VENDAS v
+ON
+	c.cpf = v.cpf_cliente
+WHERE YEAR(data_venda) = 2025;
+
+-- 3 - NOME E DO TELEFONE DE CLIENTES QUE FIZERAM COMPRAS E TENHAM DDD 61
+SELECT
+	c.nome,
+    c.telefone
+FROM
+	CLIENTES c
+INNER JOIN
+	VENDAS v
+ON c.cpf = v.cpf_cliente
+WHERE
+	c.telefone LIKE "61%";
+    
+-- 4 - VALOR DE VENDA DE TODOS OS PRODUTOS DA LG VENDIDOS NA LOJA
+SELECT
+	p.modelo,
+    p.marca,
+	v.valor
+FROM 
+	PRODUTOS p
+INNER JOIN
+	VENDAS v
+ON p.id = v.id_produto
+WHERE p.marca = "LG";
+
+-- 
+
+SELECT
+	c.nome,
+    c.telefone,
+    v.valor,
+    v.data_venda
+FROM
+	CLIENTES c
+INNER JOIN
+	VENDAS v
+ON c.cpf = v.cpf_cliente
+INNER JOIN
+	PRODUTOS p
+ON v.id_produto = p.id
+WHERE p.marca = "Sony"
+ORDER BY v.valor DESC
+;
